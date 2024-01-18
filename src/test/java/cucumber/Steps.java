3c7274db22607a6e4d87f592d02407bbc2686df2
Jacek Barnas <jacek.barnas@capgemini.com>
2024-01-18 09:20:10 +0100

    Added new tests

--- src/test/java/cucumber/Steps.java
+++ src/test/java/cucumber/Steps.java
@@ -11,12 +11,15 @@ import pageObjects.GoogleHomePage;
 import pageObjects.GoogleResultsPage;
 
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.clearBrowserCookies;
 
 public class Steps {
 
         @Given("We set base url for {string} page")
         public void openPage(String page) {
             Configuration.baseUrl = "https://" + page;
+            Configuration.headless = true;
+            clearBrowserCookies();
         }
 
         @When("We search for {string} word")
